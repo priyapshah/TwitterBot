95dfdb5c30c2f894b855639cb716ec2b88c25ae7
Priya Shah <shahpriya890@gmail.com>
2020-07-24 20:42:13 -0400

    Created a auto liker

---
+++ twitter.py
@@ -0,0 +1,24 @@
+import tweepy
+import time
+from keys import * 
+
+auth = tweepy.OAuthHandler(apiKey,apiKeySecret)
+
+auth.set_access_token(accessToken,accessTokenSecret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me();
+
+searchKey = 'UPenn'
+numTweets = 1
+
+for tweet in tweepy.Cursor(api.search, searchKey).items(numTweets):
+    try: 
+        print('Tweet Liked')
+        tweet.favorite()
+        time.sleep(10) # Creates a time gap between posts
+    except tweepy.TweepError as err: 
+        print(err.reason)
+    except StopIteration:
+        break
